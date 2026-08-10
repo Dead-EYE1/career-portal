f5f81c165930473de03e83d848234d8b7061d25b
DEAD-EYE <biplab12345yt@gmail.com>
2026-08-10 08:51:11 +0530

    ief

--- script.js
+++ script.js
@@ -191,7 +191,7 @@ window.shareOnWhatsApp = function (uid) {
   if (!job) return;
 
   const title = job.title || '';
-  const url = `https://newjobupdates.in/job/${uid}`;
+  const url = `https://newjobupdates.in/post/${uid}`;
 
   let text = '';
   if (job.section === "Scholarship") {
@@ -215,7 +215,7 @@ window.shareGeneral = function (uid) {
   const job = allData.find(j => j.uid === uid);
   if (!job) return;
   const title = job.title || 'NJ Updates';
-  const url = `https://newjobupdates.in/job/${uid}`;
+  const url = `https://newjobupdates.in/post/${uid}`;
 
   if (navigator.share) {
     navigator.share({
@@ -228,7 +228,7 @@ window.shareGeneral = function (uid) {
 };
 
 window.copyJobLink = function (uid, btnElement) {
-  const url = `https://newjobupdates.in/job/${uid}`;
+  const url = `https://newjobupdates.in/post/${uid}`;
   navigator.clipboard.writeText(url).then(() => {
     const originalHTML = btnElement.innerHTML;
     btnElement.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="20 6 9 17 4 12"></polyline></svg>Copied!`;
